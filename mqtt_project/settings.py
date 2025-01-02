16ff35c68cf6f6d4fcc163775471ae6f5218e026
PragnaShekarNav <pragna@navtat.com>
2025-01-02 17:46:37 +0530

    commit

--- mqtt_project/settings.py
+++ mqtt_project/settings.py
@@ -136,5 +136,10 @@ MQTT_PASSWORD = ''
 import os
 # STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 # STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-STATICFILES_DIRS = [BASE_DIR/'static',]
-STATIC_ROOT = BASE_DIR/'staticfiles'
+
+# STATICFILES_DIRS = [BASE_DIR/'static',]
+# STATIC_ROOT = BASE_DIR/'staticfiles'
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
